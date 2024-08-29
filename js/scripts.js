002d9e7d5e6701a4c39ea01f052b843f302dd7c9
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-08-29 19:40:18 -0400

    started the pokemon database by creating a pokemonList  in scripts.js
    the pokemonList is an array with objects that contain data of each pokemon ( name, height, and types )

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-alert('Hello World');
+let pokemonList = [
+  { name: "Abomasnow", height: 2.2, types: ["ice", "grass"] },
 
-let favoriteFood = 'lasagna';
-document.write(favoriteFood);
+  { name: "Abra", height: 0.9, types: "psychic" },
+
+  { name: "Absol", height: 1.2, types: "dark" },
+
+  { name: "Accelgor", height: 0.8, types: "bug" },
+
+  { name: "Aerodactyl", height: 1.8, types: ["rock", "flying"] },
+
+  { name: "Aggron", height: 2.1, types: ["steel", "rock"] },
+
+  { name: "Aipom", height: 0.8, types: "normal" },
+];
