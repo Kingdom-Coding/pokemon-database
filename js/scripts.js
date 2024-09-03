cdfdf43b8196ae46484ab6406b9b744cddb78320
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-09-03 10:00:00 -0400

    made the types of all the pokemon in pokemonList an array in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 let pokemonList = [
   { name: "Abomasnow", height: 2.2, types: ["ice", "grass"] },
 
-  { name: "Abra", height: 0.9, types: "psychic" },
+  { name: "Abra", height: 0.9, types: ["psychic"] },
 
-  { name: "Absol", height: 1.2, types: "dark" },
+  { name: "Absol", height: 1.2, types: ["dark"] },
 
-  { name: "Accelgor", height: 0.8, types: "bug" },
+  { name: "Accelgor", height: 0.8, types: ["bug"] },
 
   { name: "Aerodactyl", height: 1.8, types: ["rock", "flying"] },
 
   { name: "Aggron", height: 2.1, types: ["steel", "rock"] },
 
-  { name: "Aipom", height: 0.8, types: "normal" },
+  { name: "Aipom", height: 0.8, types: ["normal"] },
 ];
