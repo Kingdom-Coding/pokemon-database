c631ab19b502120e59acfa1e4ed8c12a97ca4eda
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-09-14 19:08:20 -0400

    created a IIFE for the pokemonList and 2 methods as well in the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 //array of pokemon objects and their key value pairs
-let pokemonList = [
-  { name: "Abomasnow", height: 2.2, types: ["ice", "grass"] },
+let pokemonRepository = (function() {
+  let pokemonList = [];
 
-  { name: "Abra", height: 0.9, types: ["psychic"] },
+  return {
+    getAll: (function(){
+      return pokemonList;
+    }),
+    add: (function( pokemon ){
+      pokemonList.push( pokemon );
+    }) 
+  }
+})();
 
-  { name: "Absol", height: 1.2, types: ["dark"] },
+pokemonRepository.add( 'pikachu' );
 
-  { name: "Accelgor", height: 0.8, types: ["bug"] },
-
-  { name: "Aerodactyl", height: 1.8, types: ["rock", "flying"] },
-
-  { name: "Aggron", height: 2.1, types: ["steel", "rock"] },
-
-  { name: "Aipom", height: 0.8, types: ["normal"] },
-];
-
-pokemonList.forEach(function( pokemon ) {
-  ( pokemon.height > 2.15 ) ? document.write(`<p>${pokemon.name} ${pokemon.height} 'Wow, that\'s big!'</p>`) : document.write(`<p>${pokemon.name} ${pokemon.height}</p>`)
+pokemonRepository.getAll().forEach( pokemon  => {
+  document.write(`<p>${ pokemon }</p>`);
 });
+
+console.log( pokemonRepository.getAll());
