193c962840342ad4cf1714725fc905b725d1b74b
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-09-18 17:51:01 -0400

    fixed the IIFE and the forEach loop in the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,41 @@
 //array of pokemon objects and their key value pairs
 let pokemonRepository = (function() {
-  let pokemonList = [];
+    let pokemonList = [
+      { name: "Abomasnow", height: 2.2, types: ["ice", "grass"] },
+  
+      { name: "Abra", height: 0.9, types: ["psychic"] },
+  
+      { name: "Absol", height: 1.2, types: ["dark"] },
+  
+      { name: "Accelgor", height: 0.8, types: ["bug"] },
+  
+      { name: "Aerodactyl", height: 1.8, types: ["rock", "flying"] },
+  
+      { name: "Aggron", height: 2.1, types: ["steel", "rock"] },
+  
+      { name: "Aipom", height: 0.8, types: ["normal"] },
+  ];
+
+  function add( pokemon ) {
+    return pokemonList.push( pokemon );
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
 
   return {
-    getAll: (function(){
-      return pokemonList;
-    }),
-    add: (function( pokemon ){
-      pokemonList.push( pokemon );
-    }) 
+    add: add,
+    getAll: getAll
   }
 })();
 
-pokemonRepository.add( 'pikachu' );
+pokemonRepository.getAll().forEach( pokemon => {
+  let pokemonName = pokemon.name;
+  let pokemonHeight = pokemon.height;
 
-pokemonRepository.getAll().forEach( pokemon  => {
-  document.write(`<p>${ pokemon }</p>`);
+  (pokemonHeight > 2.15) ? document.write(`<p>${pokemonName} (${pokemonHeight}) - Wow, that\'s big!</p>`) : document.write(`<p>${pokemonName} (${pokemonHeight})</p>`)
 });
 
-console.log( pokemonRepository.getAll());
+
+
