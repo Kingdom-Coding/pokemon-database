e58e990b0f0f8edb375af8baaf3524d755265ceb
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-09-25 15:35:41 -0400

    added features to display the pokemon better in scripts.js
    This was done by adding a couple new methods that display the pokemon info in a button. This button was then appended to a li that's a child of the ul element. Then an eventListener was created to display the pokemon info from the pressed btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 //array of pokemon objects and their key value pairs
 let pokemonRepository = (function() {
     let pokemonList = [
-      { name: "Abomasnow", height: 2.2, types: ["ice", "grass"] },
+      { name: "Abomasnow", height: 2.2, types: [ "ice", "grass" ] },
   
-      { name: "Abra", height: 0.9, types: ["psychic"] },
+      { name: "Abra", height: 0.9, types: [ "psychic" ] },
   
-      { name: "Absol", height: 1.2, types: ["dark"] },
+      { name: "Absol", height: 1.2, types: [ "dark" ] },
   
-      { name: "Accelgor", height: 0.8, types: ["bug"] },
+      { name: "Accelgor", height: 0.8, types: [ "bug" ] },
   
-      { name: "Aerodactyl", height: 1.8, types: ["rock", "flying"] },
+      { name: "Aerodactyl", height: 1.8, types: [ "rock", "flying" ] },
   
-      { name: "Aggron", height: 2.1, types: ["steel", "rock"] },
+      { name: "Aggron", height: 2.1, types: [ "steel", "rock" ] },
   
-      { name: "Aipom", height: 0.8, types: ["normal"] },
+      { name: "Aipom", height: 0.8, types: [ "normal" ] },
   ];
 
   function add( pokemon ) {
@@ -24,25 +24,52 @@ let pokemonRepository = (function() {
     return pokemonList;
   }
 
+  function showDetails( pokemon ) {
+      console.log( pokemon );
+    }
+
+  function customEventListener( variable, object ) {
+    return variable.addEventListener( 'click', object );
+  }
+
+  function addListItem( { name, height, types } ) {
+    //local variables
+    let pokemonBlock = document.querySelector( '.pokemon-list' );
+    let listItem = document.querySelector( '.list-item' );
+    let button = document.createElement( 'button' );
+    let newPokemon = `${ name } (${ height }) ${ types }`;
+    button.append( newPokemon );
+    
+    //appending the button to the listItem and listItem to the pokemonBlock
+    button.classList.add( 'pokemon-btn' );
+    listItem.append( button );
+    pokemonBlock.append( listItem );
+    
+
+    //eventListener to display detailedInfo when button is clicked
+    customEventListener( button, showDetails( newPokemon ));
+    return newPokemon; 
+  }
+
+  
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+    customEventListener: customEventListener
   }
 })();
 
-pokemonRepository.getAll().forEach( pokemon => {
-  let pokemonName = pokemon.name;
-  let pokemonHeight = pokemon.height; 
-  let pokemonBlock = document.querySelector( '.pokemon-list' );
-  let listItem = document.querySelector( '.list-item' );
-  let button = document.createElement('button');
 
-  button.classList.add( 'pokemon-btn' );
+pokemonRepository.add({ name: 'pikachu', height: 0.5, types: [ 'lightning' ] });
+pokemonRepository.addListItem({ name: 'squirtle', height: 0.5, types: 'water' });
 
-  listItem.append( button );
-  pokemonBlock.append( listItem );
-  buttonContent = button.append( `${pokemonName} (${pokemonHeight})` );
+pokemonRepository.getAll().forEach(function( pokemon ) {
+  pokemonRepository.addListItem( pokemon );
 })
+
 
 
 
