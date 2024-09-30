576bfeb1a2f5fcb67ba00712d3a400c03ab03bc3
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-09-30 11:19:24 -0400

    added multiple functions to the pokemonRepository and fixed the for loop to display all the pokemon in the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //array of pokemon objects and their key value pairs
-/* let pokemonRepository = (function() { */
+let pokemonRepository = (function() { 
   let pokemonList = [
   { name: "Abomasnow", height: 2.2, types: ["ice", "grass"] },
 
@@ -14,40 +14,58 @@
   { name: "Aggron", height: 2.1, types: ["steel", "rock"] },
 
   { name: "Aipom", height: 0.8, types: ["normal"] }];
+
   
-/*   return {
-    getAll: function getAll() {
-      return pokemonList;
-    },
-    add: function add({ pokemon, height }) {
-      pokemonList.push( pokemon, height );
-    }
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add( pokemon ) {
+    return pokemonList.push( pokemon );
+  }
+
+  function showDetails( pokemon ) {
+    return console.log( pokemon );
+  }
+
+  function customEventListener( element, event ) {
+    return element.addEventListener( 'click', event ); 
+  }
+
+  function addListItem( pokemon ) {
+    //get / create elements
+    let mainElement = document.querySelector( 'main' );
+    let ul = document.createElement( 'ul' );
+    let li = document.createElement( 'li' );
+    let btn = document.createElement( 'button' );
+    
+
+    let newPokemon = `${ pokemon.name } (${ pokemon.height }) ${ pokemon.types }`;
+    btn.classList.add( 'pokemon-btn' );
+    btn.innerText = newPokemon;
+
+    mainElement.append( ul );
+    ul.append( li );
+    li.append( btn );
+
+    customEventListener( btn, () => showDetails( newPokemon ));
+
+    return newPokemon;
+  }
+
+  
+   return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem
   }
 })();
 
-pokemonRepository.add();
-console.log( pokemonRepository.getAll() ); */
 
-//set global variables to access within the for loop
-/* let message = " - Wow, that's big!";
 
-//for loop to update the tallest pokemon
-for (i = 0; i < pokemonList.length; i++) {
-  let pokemonName = pokemonList[i].name;
-  let pokemonHeight = pokemonList[i].height;
+pokemonRepository.getAll().forEach( function( pokemon ) {
+  pokemonRepository.addListItem( pokemon );
+})
 
-  //conditional that writes the tallest pokemon and the message
-  if (pokemonList[i].height > 2.15) {
-    document.write( `<p>${pokemonName} (height: ${pokemonHeight}) ${message}</p>` );
-    console.log( pokemonList );
-  //writes the rest of the pokemon
-  } else {
-    document.write( `<p>${pokemonName} (height: ${pokemonHeight})</p>` );
-  }
-} */
-
-pokemonList.forEach(function( pokemon ) {
-   (pokemon.height > 2.15) ? document.write( `<p>${pokemon.name} (height: ${pokemon.height}) 'Wow, that's big!</p>`) : document.write( `<p>${pokemon.name} ${pokemon.height}</p>` );
-   console.log( pokemon );
-});
+console.log( pokemonRepository.getAll() );
 
